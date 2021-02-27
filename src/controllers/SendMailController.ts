c65145e9407e65d335fecdd7f1c3f64256427a20
Lenivene Bezerra <lenivene@msn.com>
2021-02-27 17:36:16 -0300

    fix: send email to any user

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -35,7 +35,7 @@ export class SendMailController{
     const surveys = surveysAlreadyExists;
 
     const surveysUserAlreadyExists = await surveysUserRepository.findOne({
-      where: [{user_id: user.id}, {value: null}],
+      where: {user_id: user.id, value: null},
       relations: ["user", "surveys"]
     });
 
