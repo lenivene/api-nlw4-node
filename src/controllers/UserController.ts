64eec043e25d19c08df9e94e6354b61d2c44e017
Lenivene Bezerra <lenivene@msn.com>
2021-02-26 21:35:03 -0300

    refactor: changing responsibilities

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
 
 export class UserController {
   async create(req: Request, res: Response){
     const data = req.body;
 
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UserRepository)
 
     const userAlreadyExists = await usersRepository.findOne({email: data.email});
 
